ea4436d2df71f6f15abc4f479a1e85539a57816c
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2023-01-16 17:43:03 +0100

    add foreign key to connect User to Project

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,10 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
-    protected $fillable = ['type_id', 'title', 'description', 'languages_used', 'production_date', 'slug', 'new_image'];
+    protected $fillable = ['user_id', 'type_id', 'title', 'description', 'languages_used', 'production_date', 'slug', 'new_image'];
 
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
     }
 }
